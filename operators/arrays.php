88b609cbf18ad0a6d6e087a8d7f92f69294d8bf4
Roxana Perez <roxanajpr.4@gmail.com>
2018-01-29 16:39:12 -0400

    Arrays

---
+++ operators/arrays.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roxan
+ * Date: 29/01/2018
+ * Time: 16:29
+ */
+
+$array1 = [
+    1 => 'b',
+    2 => 'c',
+    0 => 'a'
+];
+
+//$array2 = [
+  //  2 => 'b',
+    //4 => 'e'
+//];
+
+//$result = $array1 + $array2;
+//var_dump($result);
+
+$array2 = ['a', 'b', 'c'];
+var_dump($array1 == $array2);
+
+

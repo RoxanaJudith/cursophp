cc6b4d80c67b28f387faacf503aea8ab4d8c61fb
Roxana Perez <roxanajpr.4@gmail.com>
2018-01-29 16:27:27 -0400

    Comparación

---
+++ operators/comparison.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roxan
+ * Date: 29/01/2018
+ * Time: 16:11
+ */
+$v1 = 10;
+$v2 = 10;
+
+$result = $v1 < $v2;
+//$result = $v1 <=> $v2; En php 7
+
+var_dump($result);

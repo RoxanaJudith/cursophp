8701de46f51707083f2705eb4ef94f12d4a9b793
RoxanaJudith <roxanajpr.4@gmail.com>
2018-02-03 17:01:19 -0400

    Cargas de Archivos externos

---
+++ functions/external/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roxana
+ * Date: 03/02/2018
+ * Time: 16:52
+ */
+
+echo '<p>Text from function.php</p>';
+
+function sum($a, $b){
+    $result = $a + $b;
+    echo '<p>Result:'. $result . '</p>';
+}
---
+++ functions/external/index.php
@@ -0,0 +1,17 @@
+<?php
+
+//include 'functions.php';
+//include_once 'functions.php';
+require 'functions1.php';
+
+
+//require  'functions.php';
+/**
+ * Created by PhpStorm.
+ * User: roxana
+ * Date: 03/02/2018
+ * Time: 16:51
+ */
+echo '<p>Text from index.php</p>';
+
+sum(10, 20);

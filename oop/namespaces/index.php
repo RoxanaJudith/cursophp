be87f9b400be069a116cb51e8fcf558edffd197f
RoxanaJudith <roxanajpr.4@gmail.com>
2018-02-04 11:27:36 -0400

    Static

--- oop/namespaces/index.php
+++ oop/namespaces/index.php
@@ -11,15 +11,29 @@ include 'vehicles/Truck.php';
 
 //use Vehicles\Car;
 //use Vehicles\Truck;
-use Vehicles\{Car,Truck};  //Uso a partir de PHP 7
+use Vehicles\{
+    Car, Truck
+};  //Uso a partir de PHP 7
 
 
 echo 'Class Car<br>';
 $car = new Car('Alex');
-$car -> move();
-echo 'Owner car: ' . $car->getOwner().'<br>';
+$car->move();
+echo 'Owner car: ' . $car->getOwner() . '<br>';
 
-echo'<br>Class Truck<br>';
-$truck = new Truck('Maximo','Pickup');
-$truck ->move();
-echo 'Owner truck: ' . $truck->getOwner();
+echo '<br>Class Truck<br>';
+$truck = new Truck('Maximo', 'Pickup');
+$truck->move();
+echo 'Owner truck: ' . $truck->getOwner() . '<br>';
+
+//propiedades estaticas
+echo '<br>Class Truck 1<br>';
+$truck1 = new Truck('Maximo', 'Pickup');
+$truck1->move();
+
+echo '<br>Class Truck2<br>';
+$truck2 = new Truck('Maximo', 'Pickup');
+$truck2->move();
+
+//echo '<br>Total Trucks: ' . Truck::$count . '<br>';   //propiedad publica
+echo '<br>Total Trucks: ' . Truck::getTotal() . '<br>';   //propiedad privada, acceso a metodo publico

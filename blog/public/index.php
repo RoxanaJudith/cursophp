616078e34907e503d8b4db07b40a8927df561fa1
RoxanaJudith <roxanajpr.4@gmail.com>
2018-04-05 17:43:01 -0400

    Introducción a Router

--- blog/public/index.php
+++ blog/public/index.php
@@ -2,17 +2,37 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+require_once '../vendor/autoload.php';
+
 include_once  '../config.php';
 	
 $route = $_GET['route'] ?? '/';
-switch($route){
-	case '/':
-		require '../index.php';
-		break;
-	case '/admin':
-		require '../admin/index.php';
-		break;
-	case '/admin/posts'	:
-		require '../admin/posts.php';
-}
+
+use Phroute\Phroute\RouteCollector;
+
+$router = new RouteCollector();
+$router->get('/', function () use ($pdo){
+	// return 'Route /';
+	$query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
+	$query->execute();
+
+	$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
+	include '../views/index.php';
+});
+
+$dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+
+echo $response;
+// switch($route){
+// 	case '/':
+// 		require '../index.php';
+// 		break;
+// 	case '/admin':
+// 		require '../admin/index.php';
+// 		break;
+// 	case '/admin/posts'	:
+// 		require '../admin/posts.php';
+// }
 ?>
--- blog/vendor/composer/autoload_static.php
+++ blog/vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit15825aa1a987fc87f20299f055334c0b
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Phroute\\Phroute\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Phroute\\Phroute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit15825aa1a987fc87f20299f055334c0b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit15825aa1a987fc87f20299f055334c0b::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }

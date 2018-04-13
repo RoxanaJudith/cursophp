f35317a2c8296d10968bee9d8486ac14b58b13c4
RoxanaJudith <roxanajpr.4@gmail.com>
2018-04-13 08:05:31 -0400

    Templates de vistas con Twig

--- blog/app/controllers/IndexController.php
+++ blog/app/controllers/IndexController.php
@@ -4,13 +4,14 @@ namespace App\Controllers;
 /**
  *
  */
-class IndexController {
+class IndexController extends BaseController {
 	public function getIndex() {
 		global $pdo;
 		$query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
 		$query->execute();
 
 		$blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
-		return render('../views/index.php', ['blogPosts' => $blogPosts]);
+		// return $this->render('../views/index.php', ['blogPosts' => $blogPosts]);
+		return $this->render('index.twig', ['blogPosts' => $blogPosts]);
 	}
 }
